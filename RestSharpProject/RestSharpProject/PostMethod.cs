61cad7f2165ae0650012e6577dbe08ca3b3341b0
SSiddi <87267490+SSiddi@users.noreply.github.com>
2021-07-11 20:38:53 +0530

    Added post and put method test file

---
+++ RestSharpProject/RestSharpProject/PostMethod.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+
+namespace RestSharpProject
+{
+    [TestClass]
+    public class PostMethod
+    {
+       [TestMethod]
+        public void PostAPICreateUser()
+        {
+            string url = "https://petstore.swagger.io/v2/user";
+            IRestClient Restclient = new RestClient();
+            IRestRequest RestRequest = new RestRequest(url);
+            
+            JObject jObjectbody = new JObject();
+            jObjectbody.Add("username", "Adda");
+            jObjectbody.Add("firstName", "Ada");
+            jObjectbody.Add("lastName", "Maria");
+            jObjectbody.Add("email", "Ada.maria@gmail.com");
+            jObjectbody.Add("password", "test@123");
+            jObjectbody.Add("phone","02233212");
+            jObjectbody.Add("userStatus", 0);
+
+            RestRequest.AddParameter("application/json", jObjectbody, ParameterType.RequestBody);
+            IRestResponse RestResponse = Restclient.Post(RestRequest);
+            
+            System.Console.WriteLine("Create new user status code: " +RestResponse.StatusCode);
+            System.Console.WriteLine("Create new user response content: " +RestResponse.Content);
+
+            Assert.AreEqual(200, (int)RestResponse.StatusCode, "Post request failed !");
+
+        }
+
+
+
+        [TestMethod]
+        public void PostAPICreateUser_ForInvalidContentType()
+        {
+            string url = "https://petstore.swagger.io/v2/user";
+            IRestClient Restclient = new RestClient();
+            IRestRequest RestRequest = new RestRequest(url);
+
+            JObject jObjectbody = new JObject();
+            jObjectbody.Add("username", "Mohan");
+            jObjectbody.Add("firstName", "Mohan");
+            jObjectbody.Add("lastName", "Singh");
+            jObjectbody.Add("email", "MohanSingh@gmail.com");
+            jObjectbody.Add("password", "Mohan@123");
+            jObjectbody.Add("phone", "02233212");
+            jObjectbody.Add("userStatus", 0);
+
+            RestRequest.AddParameter("text/html", jObjectbody, ParameterType.RequestBody);
+            IRestResponse RestResponse = Restclient.Post(RestRequest);
+
+            System.Console.WriteLine("Create new user with invalid content type status: " +RestResponse.StatusCode);
+
+            Assert.AreEqual(415, (int)RestResponse.StatusCode, "Post request did not fail for invalid content type");
+
+        }
+    }
+}
---
+++ RestSharpProject/RestSharpProject/PutMethod.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+
+namespace RestSharpProject
+{
+    [TestClass]
+    public class PutMethod
+    {
+        [TestMethod]
+        public void PutMethodForUpdateUser()
+        {
+            string url = "https://petstore.swagger.io/v2/user/{username}";
+            IRestClient Restclient = new RestClient();
+            IRestRequest RestRequest = new RestRequest(url);
+
+            RestRequest.AddUrlSegment("username", "efwefw");
+
+            JObject jObjectbody = new JObject();
+            jObjectbody.Add("username", "Supriya");
+            jObjectbody.Add("firstName", "Supriya");
+            jObjectbody.Add("lastName", "Singh");
+            jObjectbody.Add("email", "SupriyaSingh@gmail.com");
+            jObjectbody.Add("password", "Supriya@123");
+            jObjectbody.Add("phone", "02233212");
+            jObjectbody.Add("userStatus", 0);
+
+            RestRequest.AddParameter("application/json", jObjectbody, ParameterType.RequestBody);
+            IRestResponse RestResponse = Restclient.Put(RestRequest);
+
+            System.Console.WriteLine(RestResponse.StatusCode);
+            System.Console.WriteLine(RestResponse.Content);
+            Assert.AreEqual(200, (int)RestResponse.StatusCode, "Put failed");
+        }
+
+        [TestMethod]
+        public void PutMethodWhenUserNameIsNotProvided()
+
+        {
+            string url = "https://petstore.swagger.io/v2/user/{username}";
+            IRestClient Restclient = new RestClient();
+            IRestRequest RestRequest = new RestRequest(url);
+
+            RestRequest.AddUrlSegment("username", "   ");
+
+            JObject jObjectbody = new JObject();
+            jObjectbody.Add("username", "Supriya");
+            jObjectbody.Add("firstName", "Supriya");
+            jObjectbody.Add("lastName", "Singh");
+            jObjectbody.Add("email", "SupriyaSingh@gmail.com");
+            jObjectbody.Add("password", "Supriya@123");
+            jObjectbody.Add("phone", "02233212");
+            jObjectbody.Add("userStatus", 0);
+
+            RestRequest.AddParameter("application/json", jObjectbody, ParameterType.RequestBody);
+            IRestResponse RestResponse = Restclient.Put(RestRequest);
+
+            System.Console.WriteLine(RestResponse.StatusCode);
+            System.Console.WriteLine(RestResponse.Content);
+            Assert.AreEqual(405, (int)RestResponse.StatusCode, "Put method should fail incase of no username");
+        }
+    }
+}
